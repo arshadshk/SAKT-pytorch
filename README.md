98bdd5d6fe4e220df6873e46177e1417f6d8a8c4
arshadshk7 <arshaikh5775@gmail.com>
2020-12-10 12:59:22 +0530

    model tested on kaggle auc 0.746

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# SAKT-pytorch
-Implementation of paper "A Self-Attentive model for Knowledge Tracing" 
+## SAKT-pytorch  
+Pytorch Implementation of "A Self-Attentive model for Knowledge Tracing" based on https://arxiv.org/abs/1907.06837.  
+Given the past history of student in form of questions and answers, the model tries to predict the correctness of students future questions.  
+<img src="https://github.com/arshadshk/SAKT-pytorch/from_paper.jpg">
+
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 import numpy as np
 
 
@@ -8,21 +8,26 @@ import numpy as np
 class sakt(nn.Module):  
     def __init__(self , ex_total , seq_len, dim , heads ):
         super(sakt, self).__init__()
+        self.seq_len = seq_len
+        self.dim = dim
 
-        self.embd_in = nn.Embedding( ex_total, embedding_dim  = dim )         # Interaction embedding
-        self.embd_ex = nn.Embedding( ex_total , embedding_dim = dim )       # Excercise embedding  
+        self.embd_in = nn.Embedding( 2*ex_total+1, embedding_dim  = dim )         # Interaction embedding
+        self.embd_ex = nn.Embedding( ex_total+1 , embedding_dim = dim )       # Excercise embedding  
         self.embd_pos = nn.Embedding( seq_len , embedding_dim = dim )
 
         self.linear = nn.ModuleList( [nn.Linear(in_features= dim , out_features= dim ) for x in range(3)] )   # Linear projection for each embedding 
         self.attn = nn.MultiheadAttention(embed_dim= dim , num_heads= heads, dropout= 0.2 )                                   
         self.ffn = nn.ModuleList([nn.Linear(in_features= dim , out_features=d, bias= True) for x in range(2)])  # feed forward layers post attention
 
-        self.linear_out = nn.Linear(in_features= dim , out_features= 1 , bias=True)                            # output with correctnness prediction 
+        self.linear_out = nn.Linear(in_features= dim , out_features= 1 , bias=True) 
+        self.layer_norm1 = nn.LayerNorm( dim )
+        self.layer_norm2 = nn.LayerNorm( dim )                           # output with correctnness prediction 
+        self.drop = nn.Dropout(0.2)
 
     def forward( self , input_in , input_ex):
 
         ## positional embedding
-        pos_in = self.embd_pos( torch.arange(12).unsqueeze(0) )         #making a tensor of 12 numbers, .unsqueeze(0) for converting to 2d, so as to get a 3d output #print('pos embd' , pos_in.shape)
+        pos_in = self.embd_pos( torch.arange(self.seq_len).unsqueeze(0) )         #making a tensor of 12 numbers, .unsqueeze(0) for converting to 2d, so as to get a 3d output #print('pos embd' , pos_in.shape)
 
         ## get the interaction embedding output
         out_in = self.embd_in( input_in )                         # (b, n) --> (b,n,d)
@@ -40,23 +45,20 @@ class sakt(nn.Module):
         key_in = self.linear[1](key_in).permute(1,0,2)
         query_ex =  self.linear[2](query_ex).permute(1,0,2)
 
-        n = value_in.shape[0]
-        
         ## pass through multihead attention
-        atn_out , _ = self.attn(query_ex , key_in, value_in , 
-                                attn_mask= torch.from_numpy( np.triu(np.ones((n,n)), k=1).astype('bool')) )      # lower triangular mask, bool, torch    (n,b,d)
+        atn_out , _ = self.attn(query_ex , key_in, value_in , attn_mask= torch.from_numpy( np.triu(np.ones((self.seq_len ,self.seq_len)), k=1).astype('bool')) )      # lower triangular mask, bool, torch    (n,b,d)
         atn_out = query_ex + atn_out                                  # Residual connection ; added excercise embd as residual because previous ex may have imp info, suggested in paper.
-        atn_out = nn.LayerNorm(d)( atn_out )                          # Layer norm                        #print('atn',atn_out.shape) #n,b,d = atn_out.shape
+        atn_out = self.layer_norm1( atn_out )                          # Layer norm                        #print('atn',atn_out.shape) #n,b,d = atn_out.shape
 
         #take batch on first axis 
         atn_out = atn_out.permute(1,0,2)                              #  (n,b,d) --> (b,n,d)
         
         ## FFN 2 layers
-        ffn_out = self.ffn[1]( nn.ReLU()( self.ffn[0]( atn_out ) ))   # (n,b,d) -->    .view([n*b ,d]) is not needed according to the kaggle implementation
-        ffn_out = nn.LayerNorm(d)( ffn_out + atn_out )                # Layer norm and Residual connection
+        ffn_out = self.drop(self.ffn[1]( nn.ReLU()( self.ffn[0]( atn_out ) )))   # (n,b,d) -->    .view([n*b ,d]) is not needed according to the kaggle implementation
+        ffn_out = self.layer_norm2( ffn_out + atn_out )                # Layer norm and Residual connection
 
         ## sigmoid
-        ffn_out = nn.Sigmoid()(self.linear_out( ffn_out )  )
+        ffn_out = torch.sigmoid(self.linear_out( ffn_out )  )
 
         return ffn_out
 
